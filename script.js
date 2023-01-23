f1aa0ca93a46f4bfca188958c4982f6f7e52e7dc
mamutjan turkel <mturkel@totalwine.com>
2023-01-23 16:31:25 -0500

    TT-19

--- script.js
+++ script.js
@@ -7,5 +7,7 @@ modalButton.addEventListener('click', whenClicked);
 function whenClicked() {
   console.log('Clicked!');
   modalContainer.classList.toggle('hidden');
+  const closeModalButton =
+    document.getElementsByClassName('close-modal-button');
+  console.log('closeModalButton', closeModalButton);
 }
-
