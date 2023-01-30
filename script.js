c91c69acea0cf004944fcefbfe886c9f3e7b1309
mamutjan turkel <mturkel@totalwine.com>
2023-01-30 13:11:46 -0500

    TT-68

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ console.log('Engineer Training!');
 
 const modalButton = document.getElementById('modalButton');
 const modalContainer = document.getElementById('modalContainer');
+const closeModalButton = document.getElementsByClassName('close-modal-button');
 console.log('modalButton', modalButton);
 modalButton.addEventListener('click', whenClicked);
+closeModalButton[0].addEventListener('click', whenClicked);
 function whenClicked() {
   console.log('Clicked!');
   modalContainer.classList.toggle('hidden');
-  const closeModalButton =
-    document.getElementsByClassName('close-modal-button');
   console.log('closeModalButton', closeModalButton);
 }
