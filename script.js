79ae10299a8b8bb7bf16ccbb9b553c27c0c262cf
mamutjan turkel <mturkel@totalwine.com>
2023-05-01 15:41:13 -0400

    TT-29

--- script.js
+++ script.js
@@ -29,18 +29,38 @@ const iterateJiraTitles = jiraTitles.forEach((title) => {
   console.log(title);
 });
 
+var gridContainer = document.getElementsByClassName('grid-container');
+
 let dataLoaded = false;
 
-function loadData() {
-  setTimeout(function () {
-    renderData().then((response) => {
-      dataLoaded = true;
-      gridContainer[0].innerHTML = response;
-      modalContainer.classList.toggle('hidden');
-      console.log('data loaded');
-      return response;
+const utils = {
+  renderData: function() {
+    return new Promise((resolve) => {
+      let response = '';
+      console.log('jiraObject: ', jiraObject);
+      jiraObject.forEach((object) => {
+        const { link, title } = object;
+        response += `<li>
+              <i class="bi bi-x"></i>
+              <i class="bi bi-check-circle-fill"></i>
+              <a href="${link}">${title}</a>
+            </li>`;
+      });
+      resolve(response);
     });
-  }, 1000);
+   },
+
+  loadData: function() {
+    setTimeout(function () {
+      utils.renderData().then((response) => {
+        dataLoaded = true;
+        gridContainer[0].innerHTML = response;
+        modalContainer.classList.toggle('hidden');
+        console.log('data loaded');
+        return response;
+      });
+    }, 1000);
+  }
 }
 
 modalButton.addEventListener('click', function () {
@@ -49,7 +69,7 @@ modalButton.addEventListener('click', function () {
   }
   console.log('Clicked Button!');
   modalContainer.classList.toggle('hidden');
-  loadData();
+  utils.loadData();
 });
 
 closeModalButton[0].addEventListener('click', whenClicked);
@@ -66,20 +86,5 @@ for (let i = 0; i < jiraTitles.length; i++) {
   });
 }
 
-var gridContainer = document.getElementsByClassName('grid-container');
 
-function renderData() {
-  return new Promise((resolve) => {
-    let response = '';
-    console.log('jiraObject: ', jiraObject);
-    jiraObject.forEach((object) => {
-      const { link, title } = object;
-      response += `<li>
-            <i class="bi bi-x"></i>
-            <i class="bi bi-check-circle-fill"></i>
-            <a href="${link}">${title}</a>
-          </li>`;
-    });
-    resolve(response);
-  });
-}
+
