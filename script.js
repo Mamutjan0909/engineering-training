87f5283a1e1a08ea4169512e8b72cf47c752cc91
mamutjan turkel <mturkel@totalwine.com>
2023-07-17 15:55:25 -0400

    TT-33

--- script.js
+++ script.js
@@ -18,6 +18,17 @@ const iterateJiraLinks = jiraLinks.forEach((link) => {
 
 const jiraTemplate = { icon: 'bi bi-check-circle-fill' };
 
+const errorJiraTemplate = { icon: 'bi bi-x-circle' };
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getIcon() {
+  let rNum = getRandomInt(3);
+  return rNum >= 1 ? jiraTemplate : errorJiraTemplate;
+}
+
 const jiraTitles = [
   'Create a public repository under your GitHub account',
   'Create a new script file, and import it into index.html and add a console log',
@@ -43,10 +54,11 @@ class JiraHandler {
   }
   createJiraObject() {
     for (let i = 0; i < this.titles.length; i++) {
+      let icon = getIcon();
       this.jirasObject.push({
         link: this.links[i],
         title: this.titles[i],
-        ...jiraTemplate,
+        ...icon,
       });
     }
   }
@@ -100,9 +112,10 @@ function whenClicked() {
 
 const jirasObject = [];
 for (let i = 0; i < jiraHandler.titles.length; i++) {
+  let icon = getIcon();
   jirasObject.push({
     link: jiraHandler.links[i],
     title: jiraHandler.titles[i],
-    ...jiraTemplate,
+    ...icon,
   });
 }
